723771c5556e74bac80ce8bcea5c62bb03954f49
Jayant Seth <jayant.seth-ext@arista.com>
2023-05-14 11:59:22 +0530

    small modification

--- excel_util.py
+++ excel_util.py
@@ -23,7 +23,7 @@ def get_ips():
     try:
         wb = load_workbook("details.xlsx")
         sheet = wb.active
-        row_count = sheet.max_row
+        row_count = sheet.max_row + 1
         ips = [sheet[f'A{i}'].value for i in range(2, row_count)]
         return ips
     except BaseException as e:
