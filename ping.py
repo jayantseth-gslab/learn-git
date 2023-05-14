c8185a334ee95a4ca0775ed2de8b6bea81df814d
Jayant Seth <jayant.seth-ext@arista.com>
2023-05-14 12:45:13 +0530

    minor bug fix in ping module

--- ping.py
+++ ping.py
@@ -7,7 +7,7 @@ def ping(ip):
     resp = subprocess.Popen(["ping", ip, "-c", "5"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = resp.stdout.read().decode("utf-8")
     # We will check if the output contains the string '0% packet loss' or not
-    output_list = output.split("0%\ packet\ loss")
+    output_list = output.split(" 0% packet loss")
     if len(output_list) > 1:
         return "UP"
     else:
