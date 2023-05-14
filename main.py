d841096f70ff180547cf8005788dcf8d3bc0f995
Jayant Seth <jayant.seth-ext@arista.com>
2023-05-14 12:44:17 +0530

    added time log in main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # Main file for the app
 from excel_util import get_ips, write_data
 from ping import ping_all
+from datetime import datetime as dt
 
 
 def main():
     # Get all ips from details.xlsx
+    start_time = dt.now()
     all_ips = get_ips()
     results = ping_all(all_ips)
     write_data(results)
+    end_time = dt.now()
+    print(f"Total execution time: {end_time - start_time}")
     return 0
 
 
