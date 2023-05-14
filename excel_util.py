d5b9e47448987cb3a23dab60dc72b47326813d09
Jayant Seth <jayant.seth-ext@arista.com>
2023-05-14 12:48:30 +0530

    rolled back the excel util methods

--- excel_util.py
+++ excel_util.py
@@ -1,13 +1,4 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook 
 
 
 # Method to read from excel
-def get_ips():
-    try:
-        wb = load_workbook("details.xlsx")
-        sheet = wb.active
-        row_count = sheet.max_row
-        ips = [sheet[f'A{i}'].value for i in range(2, row_count)]
-        return ips
-    except BaseException as e:
-        return e
