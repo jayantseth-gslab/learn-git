77e81bd9bd93f3c1629da25ec4689f09eed17bf2
Jayant Seth <jayant.seth94@outlook.com>
2023-05-16 17:23:02 +0530

    made ping compatible with both linux & windows

--- ping.py
+++ ping.py
@@ -1,13 +1,22 @@
 import subprocess
+import platform
 
 def ping(ip):
     """
     This method checks if the ip is reachable or not, it sends 5 ping requests
     """
-    resp = subprocess.Popen(["ping", ip, "-c", "5"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ping_list = []
+    packet_loss_string = ""
+    if (platform.system() == "Windows"):
+        ping_list = ["ping", ip, "-n", "5"]
+        packet_loss_string = "0% loss"
+    else:
+        ping_list = ["ping", ip, "-c", "5"]
+        packet_loss_string = " 0% packet loss"
+    resp = subprocess.Popen(ping_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = resp.stdout.read().decode("utf-8")
     # We will check if the output contains the string '0% packet loss' or not
-    output_list = output.split(" 0% packet loss")
+    output_list = output.split(packet_loss_string)
     if len(output_list) > 1:
         return "UP"
     else:
