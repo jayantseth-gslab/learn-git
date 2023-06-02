58964096309c1bb5e161d6ab19137a405b7b3721
Jayant Seth <jayant.seth94@gmail.com>
2023-06-02 16:38:18 +0530

    added get ips method to excel.py

--- excel_util.py
+++ excel_util.py
@@ -1,4 +1,13 @@
-from openpyxl import Workbook 
+from openpyxl import Workbook, load_workbook
 
 
 # Method to read from excel
+def get_ips():
+    try:
+        wb = load_workbook("details.xlsx")
+        sheet = wb.active
+        row_count = sheet.max_row + 1
+        ips = [sheet[f'A{i}'].value for i in range(2, row_count)]
+        return ips
+    except BaseException as e:
+        return e
